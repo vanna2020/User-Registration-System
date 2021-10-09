aa08782f07a37a50ad530da265f11d6b0dbbd009
vanna2020 <vannasingh1999@gmail.com>
2021-10-09 12:09:38 -0700

    Enter the Last Name

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -11,4 +11,13 @@ public class UserRegistration {
         Matcher matcher = pattern.matcher(firstName);
         return matcher.matches();
     }
+    /*
+    Created a method to validate last name
+     */
+    public boolean lastName(String lastName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
 }
